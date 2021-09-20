97c775c5f75fec00ee58bc5b311e120673260a81
Ezequiel Lopez <ezequiel.lopez.vallejos@gmail.com>
2021-09-20 15:17:27 -0300

    Prototipo que calcula el costo del seguro

--- js/app.js
+++ js/app.js
@@ -1,21 +1,65 @@
 // Constructores
-function Seguro(merca, anio, tipo){
+function Seguro(marca, anio, tipo) {
     this.marca = marca;
     this.anio = anio;
     this.tipo = tipo;
 }
 
-function UI(){
+// Realiza la cotización con los datos
+Seguro.prototype.cotizarSeguro = function () {
+    /**
+     * 1 = Americano 1.15
+     * 2 = Asiatico 1.05
+     * 3 = Europeo 1.35
+     */
+
+    let cantidad;
+    const base = 2000;
+
+    switch (this.marca) {
+        case '1':
+            cantidad = base * 1.15;
+            break;
+        case '2':
+            cantidad = base * 1.05;
+            break;
+        case '3':
+            cantidad = base * 1.35;
+            break;
+        default:
+            break;
+    }
+
+    // Leer el anio
+    const diferenciaAnio = new Date().getFullYear() - this.anio;
+
+    // Cada anio que la diferencia es mayor, el costo se reduce un 3%
+    cantidad -= ((diferenciaAnio * 3) * cantidad) / 100;
+
+    /**
+     * Si el seguro es basico, se multiplica por un 30% mas.
+     * Si el seguro es completo, se multiplica por un 50% mas.
+     */
+
+    if(this.tipo === 'basico'){
+        cantidad *= 1.30;
+    }else{
+        cantidad *= 1.50;
+    }
+
+    return cantidad;
 }
 
+function UI() {}
+
 // Llena las opciones de los anios
-UI.prototype.llenarOpciones = function() {
+UI.prototype.llenarOpciones = function () {
     const max = new Date().getFullYear(),
         min = max - 20;
-    
+
     const selectYear = document.querySelector('#year');
 
-    for(let i = max; i >= min; i--){
+    for (let i = max; i >= min; i--) {
         let option = document.createElement('OPTION');
         option.value = i;
         option.textContent = i;
@@ -25,12 +69,12 @@ UI.prototype.llenarOpciones = function() {
 };
 
 // Muestra alertas en pantalla
-UI.prototype.mostrarMensaje = function(mensaje, tipo) {
+UI.prototype.mostrarMensaje = function (mensaje, tipo) {
     const div = document.createElement('div');
 
-    if(tipo === 'error'){
+    if (tipo === 'error') {
         div.classList.add('error');
-    }else{
+    } else {
         div.classList.add('correcto');
     }
 
@@ -54,12 +98,13 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 registrarEventListeners();
-function registrarEventListeners(){
+
+function registrarEventListeners() {
     const formulario = document.querySelector('#cotizar-seguro');
     formulario.addEventListener('submit', cotizarSeguro);
 }
 
-function cotizarSeguro(e){
+function cotizarSeguro(e) {
     e.preventDefault();
 
     // Leer la marca seleccionada
@@ -71,11 +116,16 @@ function cotizarSeguro(e){
     // Leer el tipo de cobertura
     const tipo = document.querySelector('input[name="tipo"]:checked').value;
 
-    if(marca === '' || anio === '' || tipo === ''){
-        console.log('No pasa validacion');
+    if (marca === '' || anio === '' || tipo === '') {
         ui.mostrarMensaje('Todos los campos son obligatorios', 'error');
         return;
     }
 
     ui.mostrarMensaje('Cotizando...', 'exito');
+
+    // Instanciar el seguro
+    const seguro = new Seguro(marca, anio, tipo);
+
+    seguro.cotizarSeguro();
+    // Utilizar el prototype
 }
